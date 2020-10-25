91f415e962598ad2439f1f886f7510bf5d714add
MinimanC <casey.temple@knights.ucf.edu>
2020-10-25 13:26:39 -0400

    Challenge commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,18 +6,25 @@ using UnityEngine.UI;
 public class PlayerScript : MonoBehaviour
 {
     private Rigidbody2D rd2d;
-
     public float speed;
-
     public Text score;
-
+    public Text winText;
     private int scoreValue = 0;
+    public Text lives;
+    private int lifeValue = 3;
+    public Text loseText;
+    Animator anim;
+    private bool facingRight = true;
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
         score.text = scoreValue.ToString();
+        winText.text = "";
+        loseText.text = "";
+        lives.text = lifeValue.ToString();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -28,15 +35,73 @@ public class PlayerScript : MonoBehaviour
         rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
     }
 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            anim.SetInteger("State", 1);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            anim.SetInteger("State", 1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            anim.SetInteger("State", 2);
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            anim.SetInteger("State", 0);
+        }
+        
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown("escape"))
+        {
+            Application.Quit();
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-       if (collision.collider.tag == "Coin")
+        if (collision.collider.tag == "Enemy")
+        {
+            lifeValue -= 1;
+            lives.text = lifeValue.ToString();
+            Destroy(collision.collider.gameObject);
+            if (lifeValue <= 0)
+            {
+                loseText.text = "You Lose";
+                Destroy(this);
+            }
+        }
+        if (collision.collider.tag == "Coin")
         {
             scoreValue += 1;
             score.text = scoreValue.ToString();
             Destroy(collision.collider.gameObject);
+            if (scoreValue == 4)
+            {
+                transform.position = new Vector3(38.0f, 0.0f, 0.0f);
+                lifeValue = 3;
+                lives.text = lifeValue.ToString();
+            }
+            if (scoreValue >= 8)
+            {
+                winText.text = "Youn Win! Game created by Casey Temple";
+            }
         }
-
     }
 
     private void OnCollisionStay2D(Collision2D collision)
